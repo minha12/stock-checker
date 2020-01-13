5869509e966410cd0a8533de121294bdde734b2c
Glitch (fuchsia-session) <none>
2020-01-13 17:06:19 +0000

    🎺🔮 Checkpoint ./routes/api.js:9419671/239

--- routes/api.js
+++ routes/api.js
@@ -18,7 +18,14 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      request('https://repeated-alpaca.glitch.me/v1/stock/goog/quote', (error, response, body) => {
+      var stock = req.body.stock
+      console.log('Stock: ' + stock)
+      var like = req.body.like
+      var like_count = 0
+      if(like) {
+        like_count += 1
+      }
+      request(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`, (error, response, body) => {
         var result = JSON.parse(body)
         console.log(result)
         res.json({stockData: {stock: result.symbol, price: result.latestPrice}})
