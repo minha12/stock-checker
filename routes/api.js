d4a19db9cdf064b1f29532e4f2428f3cbaaa6ecd
Glitch (fuchsia-session) <none>
2020-01-15 08:32:51 +0000

    🏇🍈 Checkpoint ./routes/api.js:9419671/9270

--- routes/api.js
+++ routes/api.js
@@ -1,124 +1,107 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
-var fetch = require('node-fetch')
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var request = require('request')
-
+"use strict";
+var fetch = require("node-fetch");
+var expect = require("chai").expect;
+var MongoClient = require("mongodb");
+var request = require("request");
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function(app) {
+  app.route("/api/stock-prices").get(function(req, res) {
+    var stock = req.query.stock;
+    console.log("Stock: " + stock);
+    var like = req.query.like;
+    console.log(like);
+    var ip = req.connection.remoteAddress;
+    var appResponse;
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      var stock = req.query.stock
-      console.log('Stock: ' + stock)
-      var like = req.query.like
-      console.log(like)
-      var ip = req.connection.remoteAddress
-      var appResponse
-      
-      
-      
-      const stockHandler = (stock, like, ip, callback) =>{
-        return new Promise(waitForData => {
-          request(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`, (error, response, body) => {
-          var result = JSON.parse(body)
-          if(result === 'Unknown symbol') {
-            console.log('Unkown symbol')
-            callback({stockData: 'Unkown symbol'})
-          } else{
-            MongoClient.connect(CONNECTION_STRING, (err, db) => {
-              if(!like) {//check if the stock is available in db, likes remain intact
-                console.log('Connected to MongoDB')
-                db.collection('Stocks').findOneAndUpdate(
-                  {stock: stock},
-                  {
-                    $setOnInsert: {stock: stock, likes: []}
-                  },
-                  {
-                    returnOriginal: false,
-                    upsert: true
-                  },
-                  (error, data) => {
-                    //console.log(data.value)
-                    callback( {stockData: {stock: result.symbol, price: result.latestPrice, likes: data.value.likes.length}})
-                  }
-                )
-              }else{//update likes
-                db.collection('Stocks').findOneAndUpdate(
-                {stock: stock},
-                {
-                  $addToSet: {likes: ip}
-                },
-                {
-                  returnOriginal: false,
-                  upsert: true
-                },
-                (error, data) => {
-                  //console.log(data)
-                  callback({ stockData: {stock: result.symbol, price: result.latestPrice, likes: data.value.likes.length }})
-                })
-              }
-
-
-            })
+    const stockHandler = (stock, like, ip) => {
+      return new Promise(waitForData => {
+        request(
+          `https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`,
+          (error, response, body) => {
+            var result = JSON.parse(body);
+            if (result === "Unknown symbol") {
+              console.log("Unkown symbol");
+              let returnData = { stockData: "Unkown symbol" };
+              waitForData(returnData);
+            } else {
+              MongoClient.connect(CONNECTION_STRING, (err, db) => {
+                if (!like) {
+                  //check if the stock is available in db, likes remain intact
+                  console.log("Connected to MongoDB");
+                  db.collection("Stocks").findOneAndUpdate(
+                    { stock: stock },
+                    {
+                      $setOnInsert: { stock: stock, likes: [] }
+                    },
+                    {
+                      returnOriginal: false,
+                      upsert: true
+                    },
+                    (error, data) => {
+                      //console.log(data.value)
+                      var returnData = {
+                        stockData: {
+                          stock: result.symbol,
+                          price: result.latestPrice,
+                          likes: data.value.likes.length
+                        }
+                      };
+                      waitForData(returnData);
+                    }
+                  );
+                } else {
+                  //update likes
+                  db.collection("Stocks").findOneAndUpdate(
+                    { stock: stock },
+                    {
+                      $addToSet: { likes: ip }
+                    },
+                    {
+                      returnOriginal: false,
+                      upsert: true
+                    },
+                    (error, data) => {
+                      //console.log(data)
+                      var returnData = {
+                        stockData: {
+                          stock: result.symbol,
+                          price: result.latestPrice,
+                          likes: data.value.likes.length
+                        }
+                      };
+                      waitForData(returnData);
+                    }
+                  );
+                }
+              });
+            }
+            //res.json({stockData: {stock: result.symbol, price: result.latestPrice}})
           }
-          //res.json({stockData: {stock: result.symbol, price: result.latestPrice}})
+        ); //end of request
+      }); //end of Promise
+    };
 
-        })//end of request
-        })//end of Promise
+    if (Array.isArray(stock)) {
+      const stock1 = stockHandler(stock[0], like, ip);
+      const stock2 = stockHandler(stock[1], like, ip);
+      Promise.all([stock1, stock2]).then(data => {
+        console.log(data[0]);
+        console.log(data[1]);
+        stock1 = data[0]
+        stock2 = data[1]
         
-      }
-      const callBack = (data) => {
-        //console.log(data)
-        return new Promise(waitForData => {
-          waitForData(data)
-        })
-      }
-      
-      if(Array.isArray(stock)){
-        // console.log(stock[0])
-        // var myPromise1 = new Promise((resolve, reject) => {
-        //   if(true) {
-        //     resolve(stockHandler(stock[0], like, ip, callBack))
-        //   } else{
-        //     reject('Something wrong')
-        //   }
-        // })
-        // var myPromise2 = new Promise((resolve, reject) => {
-        //   if(true) {
-        //     resolve(stockHandler(stock[1], like, ip, callBack))
-        //   } else{
-        //     reject('Something wrong')
-        //   }
-        // })
-        const stock1 = stockHandler(stock[0], like, ip, callBack)
-        const stock2 = stockHandler(stock[1], like, ip, callBack)
-        Promise.all([stock1, stock2])
-          .then(data => {
-                console.log(data[0])
-                console.log(data[1])
-               })
-
-        // myPromise1.then(result => {
-        //   console.log(result)
-        // })
-        // myPromise2.then(result => {
-        //   console.log(result)
-        // })
- 
-      }
-      
-      
-    });
-    
+        
+      });
+    } //end of if Array
+  }); //end of app.route --> .get
 };
